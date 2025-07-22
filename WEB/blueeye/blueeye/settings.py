a1af0bdfa598f94e016886f8558ce8a11b5f0812
mithundinesh <midhun.mridulayam@gmail.com>
2025-07-22 17:12:34 +0530

    deployment changes

--- WEB/blueeye/blueeye/settings.py
+++ WEB/blueeye/blueeye/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blueeye.app1',
+    'app1',
     'rest_framework',
     'rest_framework.authtoken'
 
